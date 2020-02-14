1c8ec6feeaba2b62f1f83cea0616a8cb4c29c3a7
Kevin Brown <48394009+kbrowngithub@users.noreply.github.com>
2020-02-14 13:41:14 -0700

    Added handlebars tags into second.handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -51,8 +51,30 @@ module.exports = function (app) {
     app.get("/articles", function (req, res) {
         db.Article.find({})
             .then(function (dbArticle) {
-                // If all Users are successfully found, send them back to the client
-                res.json(dbArticle);
+                // console.log(`render second page with dbArticle =  ${JSON.stringify(dbArticle)}`);
+
+                var news = [];
+                //{"_id":"5e4330a22aeae84b50e87e97","title":"FP: The good parts","link":"https://codingwithjs.rocks/blog/fp-the-good-parts","__v":0}
+                console.log(`First Article: ${dbArticle[0].title}\n${dbArticle[0].link}`)
+                var uData = {title:"Title 1",link:"http://somelink"};
+
+                // if (r.length === 0) {
+                //     uData = {
+                //         name: dbArticle[0].name
+                //     };
+                // } else {
+                //     for (var i = 0; i < r.length; i++) {
+                //         console.log(`news name = ${JSON.stringify(dbArticle[0].SavedRestaurants[i].name)}`);
+                //         news.push({ name: dbArticle[0].SavedRestaurants[i].name });
+                //         console.log(`news[${i}] = ${news[i].name}`);
+                //     }
+                //     uData = {
+                //         name: dbArticle[0].name,
+                //         news: news
+                //     };
+                // }
+
+                return res.render("second", {newsArticle: dbArticle});
             })
             .catch(function (err) {
                 // If an error occurs, send the error back to the client
@@ -62,7 +84,7 @@ module.exports = function (app) {
 
     // Route for grabbing a specific Article by id, populate it with it's note
     app.get("/articles/:id", function (req, res) {
-        db.Article.findONe({ _id: req.params.id })
+        db.Article.findOne({ _id: req.params.id })
             // Specify that we want to populate the retrieved libraries with any associated books
             .populate("note")
             .then(function (dbArticle) {
